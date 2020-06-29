fcc3dc2684282bafbcb1117cd819e829c6c5aee0
willhitch <hitch.will@gmail.com>
2020-06-28 22:26:46 -0600

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+    id INT
+    AUTO_INCREMENT,
+    burger_name VARCHAR
+    (255),
+    devoured BOOLEAN,
+    PRIMARY KEY
+    (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Bacon Cheeseburger", false );
+INSERT INTO burgers (burger_name, devoured) VALUES ("A Royal With Cheese", false );
+INSERT INTO burgers (burger_name, devoured) VALUES ("BLT With burger sauce", true );
