e3473c5d40aad635ed2f8dce07993d895b8625dd
willhitch <hitch.will@gmail.com>
2020-06-30 15:03:07 -0600

    added Jquery ajax calls and file linking

---
+++ public/assets/js/burger.js
@@ -0,0 +1,43 @@
+$(function () {
+  $(".change-devoured").on("click", function (event) {
+    event.preventDefault()
+
+    var burgerId = event.currentTarget.attributes[1].nodeValue
+    console.log(burgerId)
+
+    var isDevoured = {
+      devoured: true,
+    }
+
+    $.ajax("/api/burgers/" + burgerId, {
+      type: "PUT",
+      data: isDevoured,
+    }).then(function (res) {
+      console.log(res)
+      location.reload()
+    })
+  })
+
+  $(".creat-form").on("submit", function (event) {
+    event.preventDefault()
+
+    var isDevoured
+
+    $("input[type='radio']").change(function () {
+      isDevoured = $(this).val()
+    })
+
+    var burger = {
+      burger_name: $("#bur").val().trim(),
+      devoured: isDevoured,
+    }
+
+    $.ajax("/api/burgers", {
+      type: "PUT",
+      data: burger,
+    }).then(function (res) {
+      console.log(res)
+      location.reload()
+    })
+  })
+})
--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-// Import Dependencies 
+// Import Dependencies
 var express = require("express")
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000
 
-var app = express();
+var app = express()
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use(express.static("public"))
 
 // Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 // Set Handlebars.
-var exphbs = require("express-handlebars");
+var exphbs = require("express-handlebars")
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }))
+app.set("view engine", "handlebars")
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");
+var routes = require("./controllers/burgers_controller.js")
 
-app.use(routes);
+app.use(routes)
 
-app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
-});
+app.listen(PORT, function () {
+  console.log("App now listening at localhost:" + PORT)
+})
